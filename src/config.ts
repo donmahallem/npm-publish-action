c3cb8638df45a767970b6fa7db6d263726cd7a71
donmahallem <donmahallem@users.noreply.github.com>
2019-09-24 22:31:21 +0200

    bump

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export interface Config {
     TARGET: string,
-    GITHUB_SECRET: string
+    GITHUB_SECRET: string | false,
+    NPM_SECRET: string | false,
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as github from '@actions/github';
 import * as actionscore from '@actions/core';
+import { writeFileSync } from 'fs';
 import { Config } from './config';
 
 const config: Config = {
@@ -7,45 +8,48 @@ const config: Config = {
         required: false
     }),
     GITHUB_SECRET: actionscore.getInput("github_secret", {
-        required: true
+        required: false
+    }),
+    NPM_SECRET: actionscore.getInput("npm_secret", {
+        required: false
     })
 };
-console.log("filter: ", config.FILTER)
-console.log(github.context.action, github.context.eventName);
-async function runa() {
-    if (github.context.action.localeCompare('pull_request')) {
-
-
-        if (github.context.payload.pull_request) {
-            const prData: any = github.context.payload.pull_request;
-            const githubClient: any = new github.GitHub(config.GITHUB_SECRET);
-            const data = await githubClient.checks.create({
-                owner: prData.user.login,
-                repo: prData.head.repo.name,
-                name: "require-label",
-                head_sha: prData.head.sha,
-                conclusion: prData.labels.length > 0 ? 'success' : 'failure'
-            })
-            const data2 = await githubClient.repos.requestPageBuild({
-                owner: prData.user.login,
-                repo: prData.head.repo.name
-            })
-            console.log(JSON.stringify(data))
-            console.log(JSON.stringify(data2))
-        } else {
-
-            console.log("test");
-            const payload = JSON.stringify(github.context.payload, undefined, 2);
-            console.log(payload);
-            actionscore.setOutput("T1", "T2");
-            actionscore.setFailed("Bommel");
-        }
-    } else {
-        actionscore.setFailed('This action can only be used on pull requests');
+const writeFile = (file: string, data: string) => {
+    return writeFileSync(file, data, { encoding: "utf-8" });
+}
+const setupNpmrcFile = (domain: string, user: string, secret: string) => {
+    const data: string = "//" + domain + "/:_authToken=" + secret
+        + "\nregistry=https://" + domain + "/" + user;
+}
+const deployToGithub = (config: Config) => {
+    actionscore.info('Deploying to Github');
+    if (config.NPM_SECRET === false) {
+        actionscore.setFailed('Github Secret required');
+        return;
+    }
+}
+const deployToNpmjs = (config: Config) => {
+    actionscore.info('Deploying to NPMJS');
+    if (config.NPM_SECRET === false) {
+        actionscore.setFailed('NPM Secret required');
+        return;
+    }
+}
+async function runa(config: Config) {
+    switch (config.TARGET) {
+        case 'npmjs':
+            deployToNpmjs(config);
+            break;
+        case 'github':
+            deployToGithub(config);
+            break;
+        default:
+            actionscore.setFailed("Target required!");
+            break;
     }
 }
 
-runa().catch((err) => {
+runa(config).catch((err) => {
     console.error(err);
     actionscore.setFailed("Error");
 }).then(() => {
