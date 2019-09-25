05fdb6bca36c68b80cf3325347ef5dbfa832662c
donmahallem <donmahallem@users.noreply.github.com>
2019-09-25 16:53:43 +0200

    fix non correct exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ elif [ "$INPUT_TARGET" = "npm" ]; then
   npm config set "//registry.npmjs.org/:_authToken" "$NPM_SECRET"
   npm publish -reg "https://registry.npmjs.org" $INPUT_PUBLISH_ARGS
   
-  exit 1
+  exit 0
 elif [ "$INPUT_TARGET" = "github" ]; then
   echo "Target Github defined"
   if [ -z "$INPUT_GITHUB_SECRET" ]; then
@@ -24,7 +24,7 @@ elif [ "$INPUT_TARGET" = "github" ]; then
   #npm config set "registry" "https://npm.pkg.github.com/$GITHUB_ACTOR" --userconfig ./.npmrc
   npm config set "//npm.pkg.github.com/:_authToken" "$GITHUB_SECRET"
   npm publish -reg "https://npm.pkg.github.com/$GITHUB_ACTOR" $INPUT_PUBLISH_ARGS
-  exit 1
+  exit 0
 else
   echo "Target '$INPUT_TARGET' is unknown! Use either 'github' or 'npm'."
   exit 1
